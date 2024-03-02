643b4df64a75ead9e36d6d2a5cc69f4988f26cd2
Daemyeong Jeon <dajeon@c5r1s7.42seoul.kr>
2024-03-02 21:23:18 +0900

    ex02: main

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -17,7 +17,7 @@ int main()
 	mstack.push(17);
 	std::cout << mstack.top() << std::endl;
 	mstack.pop();
-	std::cout << mstack.size() << std::endl;
+	std::cout << "size: " << mstack.size() << std::endl;
 	std::cout << mstack.top() << std::endl;
 
 	mstack.push(3);
@@ -39,7 +39,7 @@ int main()
 		std::cout << *it << std::endl;
 		++it;
 	}
-	std::cout << "= mstack2 ===============" << std::endl;
+	std::cout << "= mstack2(const) ========" << std::endl;
 	MutantStack<int>::const_iterator it2 = mstack2.begin();
 	MutantStack<int>::const_iterator ite2 = mstack2.end();
 	while (it2 != ite2) {
@@ -60,7 +60,7 @@ int main()
 		std::cout << *rit << std::endl;
 		++rit;
 	}
-	std::cout << "= mstack2(r) =============" << std::endl;
+	std::cout << "= mstack2(r, const) ====" << std::endl;
 	MutantStack<int>::const_reverse_iterator rit2 = mstack2.rbegin();
 	MutantStack<int>::const_reverse_iterator rite2 = mstack2.rend();
 	while (rit2 != rite2) {
